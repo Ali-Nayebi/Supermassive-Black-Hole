bcb37a5fe18467c838ee6052a483e4eb0a95c38e
Ali Nayebi <elderekkoo@gmail.com>
2023-08-19 19:22:18 +0330

    transition on members section

--- client/src/portfolio/scenes/Members.jsx
+++ client/src/portfolio/scenes/Members.jsx
@@ -1,9 +1,13 @@
 import { motion } from 'framer-motion';
 import LineGradient from '../components/LineGradient';
+import './members.css';
 
 const Members = ({ setSelectedPage }) => {
+  const overlayStyles = `absolute opacity-0 hover:opacity-90 transition duration-500
+    bg-grey z-30 flex flex-col justify-center items-center text-center text-deep-blue`;
   return (
     <section id="members" className="pt-10 pb-24">
+      {/* header */}
       <motion.div
         className="md:w-2/4 mx-auto text-center"
         initial="hidden"
@@ -26,27 +30,40 @@ const Members = ({ setSelectedPage }) => {
       </motion.div>
 
       <div className="md:flex md:justify-between mt-5 gap-10">
+        {/* 1st */}
         <motion.div
-          className="md:w-1/3 mt-10"
+          className="md:w-1/3 mt-10 relative"
           initial="hidden"
           whileInView="visible"
           onViewportEnter={() => setSelectedPage('members')}
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.2, duration: 0.5 }}
+          transition={{ duration: 0.5 }}
           variants={{
             hidden: { opacity: 0, x: -50 },
             visible: { opacity: 1, x: 0 },
           }}
         >
-          <div className="text-center z-10 bg-black">
-            <p className="font-playfair p-5 font-semibold text-4xl">
+          <div className="text-center z-10 ">
+            <p className="font-playfair pt-1 px-5 font-semibold text-4xl">
               Ali Nayebi
             </p>
-            <img
-              alt="Ali Nayebi"
-              className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]"
-              src="assets/ali.jpg"
-            />
+            <p className="font-playfair p-1 font-semibold text-xl">
+              Front-End Developer
+            </p>
+            <div className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]">
+              <div className={overlayStyles}>
+                <img
+                  alt="Ali Nayebi"
+                  className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]"
+                  src="assets/alii.JPG"
+                />
+              </div>
+              <img
+                alt="Ali Nayebi"
+                className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]"
+                src="assets/ali.jpg"
+              />
+            </div>
 
             <p className="font-playfair font-semibold text-3xl mt-3">
               Experience
@@ -54,26 +71,38 @@ const Members = ({ setSelectedPage }) => {
           </div>
         </motion.div>
         <motion.div
-          className="md:w-1/3 mt-10"
+          className="md:w-1/3 mt-10 relative"
           initial="hidden"
           whileInView="visible"
           onViewportEnter={() => setSelectedPage('members')}
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.4, duration: 0.5 }}
+          transition={{ duration: 0.5 }}
           variants={{
             hidden: { opacity: 0, x: -50 },
             visible: { opacity: 1, x: 0 },
           }}
         >
-          <div className="text-center z-10 bg-black">
-            <p className="font-playfair p-5 font-semibold text-5xl">
+          <div className="text-center z-10 ">
+            <p className="font-playfair pt-1 px-5 font-semibold text-4xl">
               Ali Nayebi
             </p>
-            <img
-              alt="Ali Nayebi"
-              className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]"
-              src="assets/ali.jpg"
-            />
+            <p className="font-playfair p-1 font-semibold text-xl">
+              Front-End Developer
+            </p>
+            <div className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]">
+              <div className={overlayStyles}>
+                <img
+                  alt="Ali Nayebi"
+                  className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]"
+                  src="assets/alii.JPG"
+                />
+              </div>
+              <img
+                alt="Ali Nayebi"
+                className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]"
+                src="assets/ali.jpg"
+              />
+            </div>
 
             <p className="font-playfair font-semibold text-3xl mt-3">
               Experience
@@ -81,26 +110,38 @@ const Members = ({ setSelectedPage }) => {
           </div>
         </motion.div>
         <motion.div
-          className="md:w-1/3 mt-10"
+          className="md:w-1/3 mt-10 relative"
           initial="hidden"
           whileInView="visible"
           onViewportEnter={() => setSelectedPage('members')}
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.6, duration: 0.5 }}
+          transition={{ duration: 0.5 }}
           variants={{
             hidden: { opacity: 0, x: -50 },
             visible: { opacity: 1, x: 0 },
           }}
         >
-          <div className="text-center z-10 bg-black">
-            <p className="font-playfair p-5 font-semibold text-5xl">
+          <div className="text-center z-10 ">
+            <p className="font-playfair pt-1 px-5 font-semibold text-4xl">
               Ali Nayebi
             </p>
-            <img
-              alt="Ali Nayebi"
-              className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]"
-              src="assets/ali.jpg"
-            />
+            <p className="font-playfair p-1 font-semibold text-xl">
+              Front-End Developer
+            </p>
+            <div className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]">
+              <div className={overlayStyles}>
+                <img
+                  alt="Ali Nayebi"
+                  className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]"
+                  src="assets/alii.JPG"
+                />
+              </div>
+              <img
+                alt="Ali Nayebi"
+                className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]"
+                src="assets/ali.jpg"
+              />
+            </div>
 
             <p className="font-playfair font-semibold text-3xl mt-3">
               Experience
@@ -108,26 +149,38 @@ const Members = ({ setSelectedPage }) => {
           </div>
         </motion.div>
         <motion.div
-          className="md:w-1/3 mt-10"
+          className="md:w-1/3 mt-10 relative"
           initial="hidden"
           whileInView="visible"
           onViewportEnter={() => setSelectedPage('members')}
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.8, duration: 0.5 }}
+          transition={{ duration: 0.5 }}
           variants={{
             hidden: { opacity: 0, x: -50 },
             visible: { opacity: 1, x: 0 },
           }}
         >
-          <div className="text-center z-10 bg-black">
-            <p className="font-playfair p-5 font-semibold text-5xl">
+          <div className="text-center z-10 ">
+            <p className="font-playfair pt-1 px-5 font-semibold text-4xl">
               Ali Nayebi
             </p>
-            <img
-              alt="Ali Nayebi"
-              className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]"
-              src="assets/ali.jpg"
-            />
+            <p className="font-playfair p-1 font-semibold text-xl">
+              Front-End Developer
+            </p>
+            <div className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]">
+              <div className={overlayStyles}>
+                <img
+                  alt="Ali Nayebi"
+                  className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]"
+                  src="assets/alii.JPG"
+                />
+              </div>
+              <img
+                alt="Ali Nayebi"
+                className="z-10 w-full max-w-[400px] m-auto md:max-w-[600px]"
+                src="assets/ali.jpg"
+              />
+            </div>
 
             <p className="font-playfair font-semibold text-3xl mt-3">
               Experience
