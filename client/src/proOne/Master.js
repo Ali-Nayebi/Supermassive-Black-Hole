7a697b73e97ef5b52adb39b63abf32c681964a91
Ali Nayebi <elderekkoo@gmail.com>
2023-08-27 17:34:24 +0330

    project1 navbar

--- client/src/proOne/Master.js
+++ client/src/proOne/Master.js
@@ -1,5 +1,25 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Navbar from './scenes/global/Navbar';
+import './master.css';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 function Master() {
-  return <div className="app">hey</div>;
+  return (
+    <div className="appp">
+      <Navbar />
+      <ScrollToTop />
+    </div>
+  );
 }
 
 export default Master;
